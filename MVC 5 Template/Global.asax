e9454c6617a5627d4cf8ed0a78e77e6c6568064e
Bryce Hattamer <bhattamer@akaio.com>
2017-05-23 09:26:11 -0400

    blank MVC project

---
+++ MVC 5 Template/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_5_Template.MvcApplication" Language="C#" %>
---
+++ MVC 5 Template/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_5_Template.Startup))]
+namespace MVC_5_Template
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
